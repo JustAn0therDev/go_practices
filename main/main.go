2d9e1401ad4556fcb922ca6fb40460e0bc664bbb
Ruan Montelo <ruanmontelo@gmail.com>
2021-03-28 03:15:49 -0300

    Adding values being read from STDIN and dealing with structs and pointers

--- main/main.go
+++ main/main.go
@@ -1,39 +1,63 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	"example.com/calc"
 )
 
+type Calculation struct {
+	operation string
+	first int
+	second int
+	result int
+	error error
+}
+
 func main() {
-	sum, err := calc.Sum(1, 2)
+	// Although Golang has pointers, there is no need to free allocated memory
+	// for a pointer
+	var calculation Calculation = Calculation{}
+	fmt.Println("please select an operation (sum, sub, mult or div): ")
+	fmt.Scanln(&calculation.operation)
 
-	if err != nil {
-		log.Fatalln(err)
+	fmt.Println("insert the first value: ")
+	fmt.Scanln(&calculation.first)
+
+	fmt.Println("insert the second value: ")
+	fmt.Scanln(&calculation.second)
+
+	calculation.result, calculation.error = callCalculation(calculation.first, calculation.second, &calculation.operation)
+
+	if calculation.error != nil {
+		log.Fatalln(calculation.error)
 	}
 
-	sub, err := calc.Sub(1, 2)
+	fmt.Println(calculation.result)
+}
 
-	if err != nil {
-		log.Fatalln(err)
+func callCalculation(numberOne int, numberTwo int, op *string) (int, error) {
+	result := 0
+	var err error
+
+	switch *op {
+		case "sum":
+			result, err = calc.Sum(numberOne, numberTwo)
+		case "sub":
+			result, err = calc.Sub(numberOne, numberTwo)
+		case "mult":
+			result, err = calc.Mult(numberOne, numberTwo)
+		case "div":
+			result, err = calc.Div(numberOne, numberTwo)
+		default:
+			return 0, errors.New("unknown operation")
 	}
 
-	mult, err := calc.Mult(1, 2)
-
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
-	div, err := calc.Div(1, 2)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println(sum)
-	fmt.Println(sub)
-	fmt.Println(mult)
-	fmt.Println(div)
+	return result, nil
 }
